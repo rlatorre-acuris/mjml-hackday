a66d57f393b76a5c3824cd049d3d58eda8ced5a6
Riccardo La Torre <riccardo.latorre@acuris.com>
2020-05-21 15:57:41 +0100

    Static example of mm-headlines

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 8080
+
+app.use('/img', express.static('img'))
+
+app.get('/', (req, res) => res.sendFile(path.resolve('./index.html')))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
